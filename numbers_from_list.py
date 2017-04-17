b303145b58fc925412f7fd25a4e7d53adf50336f
takutico <takutico@yahoo.es>
2017-04-17 17:43:26 +1000

    add reverse_list.py

--- numbers_from_list.py
+++ numbers_from_list.py
@@ -5,4 +5,14 @@ L = ['asfd', '1', '44','g', '4 5', '7k', '563']
 # 	if i.isdigit():
 # 		tl.append(i)
 
-print([i for i in l if i.isdigit()])
+# using for loop
+print('Using loop')
+print([i for i in L if i.isdigit()])
+
+import re
+
+print('Using regular expressions')
+s = '123fdsf24rt3g4'
+m = re.findall(r'\d+', s)
+print(m)
+# print(x for x in re.match(r'\d+', s))
